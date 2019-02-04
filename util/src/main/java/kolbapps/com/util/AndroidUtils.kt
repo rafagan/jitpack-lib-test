ed72c742502c00d51fabc7661bdcaac7cbe4b869
Rafagan Abreu <rafagansa@outlook.com>
2019-02-04 11:12:59 -0200

    Teste inicial

---
+++ util/src/main/java/kolbapps/com/util/AndroidUtils.kt
@@ -0,0 +1,12 @@
+package kolbapps.com.util
+
+import android.util.Log
+
+class AndroidUtils {
+    companion object {
+        fun teste() {
+            Log.d("Teste lib", "Funcionou!!!1")
+        }
+    }
+
+}
